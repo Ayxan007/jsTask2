db4f53757f6b3bd7fc6925f6c128bd827021e9c7
AyxanR <ahuseynli630@gmail.com>
2024-12-05 19:00:51 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// function checkSeason(month) {
+//   switch (month) {
+//     case "dekabr":
+//     case "yanvar":
+//     case "fevral":
+//       return "Qış fəsli";
+//     case "mart":
+//     case "aprel":
+//     case "may":
+//       return "Yaz fəsli";
+//     case "iyun":
+//     case "iyul":
+//     case "avqust":
+//       return "Yay fəsli";
+//     case "sentyabr":
+//     case "oktaybr":
+//     case "noyabr":
+//       return "Payız fəsli";
+
+//     default:
+//       return "Namelum Ay";
+//   }
+// }
+
+// console.log(checkSeason("sentyabr"));
+
+
+// function checkSpeed(speed){
+
+//     const limit = 70;
+//     const sum = speed - limit;
+
+//     switch(true){
+//         case speed <=70:
+//             return "Suret mehdudiyyete uygundur"
+//             case speed > 70 && speed <= 100:
+//                 return `Suret heddini asdiniz ${sum} AZN`
+//                 case speed > 100:
+//                     return "Tehlukeli suretdir 200 azn cerime"
+//     } 
+// }
+
+
+
+// console.log(checkSpeed(112));
+
+
+// function checkPerson(hour){
+//     const dayHour = 25;
+//     const fullHour = 8;
+//     const addHour = 25;
+//     const sum = dayHour / fullHour;
+//     if(hour <= fullHour){
+//         return dayHour;
+//     }else{
+//         const addHoursPrice = hour - fullHour;
+//         return dayHour + addHoursPrice * addHour;
+//     }
+// }
+
+// console.log(checkPerson(11));
+
+
+
+// function checkZodiac(month, day){
+//     if(day)
+// }
